f8114f27af6385557bbf7f3eef4442a7efb1669d
Eric Kryski <e.kryski@gmail.com>
2012-11-02 00:35:02 -0600

    updated it to work on desktops with touch enabled and mobile devices

--- js/requestAnimationFrame.js
+++ js/requestAnimationFrame.js
@@ -6,34 +6,34 @@
 
 ( function () {
 
-	var lastTime = 0;
-	var vendors = [ 'ms', 'moz', 'webkit', 'o' ];
+  var lastTime = 0;
+  var vendors = [ 'ms', 'moz', 'webkit', 'o' ];
 
-	for ( var x = 0; x < vendors.length && !window.requestAnimationFrame; ++ x ) {
+  for ( var x = 0; x < vendors.length && !window.requestAnimationFrame; ++ x ) {
 
-		window.requestAnimationFrame = window[ vendors[ x ] + 'RequestAnimationFrame' ];
-		window.cancelAnimationFrame = window[ vendors[ x ] + 'CancelAnimationFrame' ] || window[ vendors[ x ] + 'CancelRequestAnimationFrame' ];
+    window.requestAnimationFrame = window[ vendors[ x ] + 'RequestAnimationFrame' ];
+    window.cancelAnimationFrame = window[ vendors[ x ] + 'CancelAnimationFrame' ] || window[ vendors[ x ] + 'CancelRequestAnimationFrame' ];
 
-	}
+  }
 
-	if ( !window.requestAnimationFrame ) {
+  if ( !window.requestAnimationFrame ) {
 
-		window.requestAnimationFrame = function ( callback, element ) {
+    window.requestAnimationFrame = function ( callback, element ) {
 
-			var currTime = Date.now(), timeToCall = Math.max( 0, 25 - ( currTime - lastTime ) );
-			var id = window.setTimeout( function() { callback( currTime + timeToCall ); }, timeToCall );
-			lastTime = currTime + timeToCall;
-			return id;
+      var currTime = Date.now(), timeToCall = Math.max( 0, 25 - ( currTime - lastTime ) );
+      var id = window.setTimeout( function() { callback( currTime + timeToCall ); }, timeToCall );
+      lastTime = currTime + timeToCall;
+      return id;
 
-		};
+    };
 
-	}
+  }
 
 
-	if ( !window.cancelAnimationFrame ) {
+  if ( !window.cancelAnimationFrame ) {
 
-		window.cancelAnimationFrame = function ( id ) { clearTimeout( id ); };
+    window.cancelAnimationFrame = function ( id ) { clearTimeout( id ); };
 
-	}
+  }
 
 }() );
